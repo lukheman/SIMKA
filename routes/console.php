bbb0849b06adff45d2ea2a00c32419191e2f5dea
Akmal <akmalthemalong@gmail.com>
2026-04-05 22:32:43 +0800

    feat: fitur pengingat pembayaran

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Kirim notifikasi pengingat angsuran setiap hari
+Schedule::command('app:send-angsuran-reminders')->daily();
