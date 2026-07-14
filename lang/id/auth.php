72ae0891c1c67d1484ce903129032dd0f85d525e
Akmal <akmalthemalong@gmail.com>
2026-07-14 21:34:42 +0800

    feat(lang): tambah file bahasa Indonesia untuk autentikasi, pagination, pengaturan password, dan validasi

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Kredensial yang diberikan tidak cocok dengan data kami.',
+    'password' => 'Password yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silakan coba lagi dalam :seconds detik.',
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset'     => 'Password Anda sudah direset!',
+    'sent'      => 'Kami sudah mengirimkan tautan reset password ke email Anda!',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token'     => 'Token reset password ini tidak valid.',
+    'user'      => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+];
